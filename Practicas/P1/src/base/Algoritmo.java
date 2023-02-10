89214f2b0980fd61a31e8d274ba3634cd7d60fab
yojhansg <yojhansg@ucm.es>
2023-02-10 15:28:53 +0100

    Clases genericas

--- Practicas/P1/src/base/Algoritmo.java
+++ Practicas/P1/src/base/Algoritmo.java
@@ -1,5 +1,17 @@
 package base;
 
-public class Algoritmo {
+public abstract class Algoritmo {
 
+	
+	private int tamPoblacion;
+	private Individuo[] poblacion;
+	private double[] fitness;
+	private int maxGeneraciones;
+	private double probCruce;
+	private double probMutacion;
+	private int tamTorneo;
+	private Individuo elMejor;
+	private int pos_mejor;
+	
 }
+
---
+++ Practicas/P1/src/base/Individuo.java
@@ -0,0 +1,8 @@
+package base;
+
+public abstract class Individuo<T> {
+
+	
+	T[] cromosoma;
+	int[] tamGenes;
+}
