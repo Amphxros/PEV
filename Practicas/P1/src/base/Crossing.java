e62242f93acfd67a0e2dd2cfdfff7965d42fa929
Amph Rubio Bellón <ampxrubio@gmail.com>
2023-02-16 16:46:18 +0100

    Cruce monopunto

--- Practicas/P1/src/base/Crossing.java
+++ Practicas/P1/src/base/Crossing.java
@@ -4,7 +4,19 @@ public class Crossing {
 	public enum Type{
 		Mono, Multiple, Uniform
 	}
-	public void MonopointCrossOver() {
+	public <T> void MonopointCrossOver(Individuo<T> parentA, Individuo<T> parentB, Individuo<T>childA, Individuo<T> childB, int crossOverPoint) {
+		// first part: A to childA and B to childB
+		for(int i=0;i<crossOverPoint; i++) {
+			//cross children
+			childA.crossOver(parentA, i);
+			childB.crossOver(parentB, i);
+		}
+		
+		//second part:  A to childB and B to childA
+		for(int j=crossOverPoint; j< parentA.getCromosomes().length; j++) {
+			childA.crossOver(parentB, j);
+			childB.crossOver(parentA, j);
+		}
 		
 	}
 	public void MultiPointCrossOver() {
