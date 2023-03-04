a7ba04645b58b834064c17b44a4667c27aae8da2
Amph Rubio Bellón <ampxrubio@gmail.com>
2023-03-04 05:28:29 +0100

    RealGen

---
+++ Practicas/P1G11/src/Common/Genes/RealGen.java
@@ -0,0 +1,11 @@
+package Common.Genes;
+
+public class RealGen extends Gen<Double>{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return allele.toString();
+	}
+
+}
